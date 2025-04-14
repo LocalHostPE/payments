038cd8d0a007c1447ef4b923de13df12926e2cae
LocalHostPE <marlon098gamer890@gmail.com>
2025-04-13 22:17:15 -0500

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // --- Configuración ---
-const AUTH_URL = 'https://votrox.vercel.app/auth.html'; // URL de la página de autenticación
+const AUTH_URL = 'https://votrox.vercel app/auth.html'; // URL de la página de autenticación
 const SYSTEM_ID = '2yc2mNVsnt2v0OA6TWjaueem11PdcSqJ'; // Reemplaza con el System ID específico de la empresa
+const ENTERPRISE_NAME = 'VxGroup-SolucionesTecnologicas'; // Reemplaza con el nombre de la empresa
 
 function generarTokenUnico(length = 100) {
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_';
@@ -11,15 +12,16 @@ function generarTokenUnico(length = 100) {
     return token;
 }
 
-function construirURLAutenticacion(redirectURL, systemID, oneTimeToken) {
+function construirURLAutenticacion(redirectURL, systemID, oneTimeToken, enterpriseName) {
     const encodedRedirectURL = encodeURIComponent(redirectURL);
-    return `${AUTH_URL}?redirecturl=${encodedRedirectURL}&systemID=${systemID}&verify=${oneTimeToken}`;
+    const encodedEnterpriseName = encodeURIComponent(enterpriseName);
+    return `${AUTH_URL}?redirecturl=${encodedRedirectURL}&systemID=${systemID}&verify=${oneTimeToken}&enterprice=${encodedEnterpriseName}`;
 }
 
 // --- Generación del Token y Construcción de la URL ---
 const redirectURL = 'https://tu_aplicacion.netlify.app/panel'; // URL a la que se redirigirá después del login
 const oneTimeToken = generarTokenUnico(); // Genera un token de 100 caracteres
-const urlDeAutenticacion = construirURLAutenticacion(redirectURL, SYSTEM_ID, oneTimeToken);
+const urlDeAutenticacion = construirURLAutenticacion(redirectURL, SYSTEM_ID, oneTimeToken, ENTERPRISE_NAME);
 
 console.log('URL de Autenticación Generada:');
 console.log(urlDeAutenticacion);
